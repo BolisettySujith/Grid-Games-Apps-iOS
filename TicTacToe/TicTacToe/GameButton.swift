98d9e130efd9163eabf7a16919121518115c7d19
BolisettySujith <bsujith116@gmail.com>
2024-10-27 16:45:59 +0530

    adds tic tac toe game

---
+++ TicTacToe/TicTacToe/GameButton.swift
@@ -0,0 +1,20 @@
+//
+//  GameButton.swift
+//  TicTacToe
+//
+//  Created by Havells on 27/10/24.
+//
+
+import UIKit
+
+class GameButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
